a9362beffc62c45d8ef1b0a77e367b2d38b227a4
CoryStory <cwyman1@asu.edu>
2016-09-20 16:46:18 -0700

    Just some rough placeholder stuff
    Can and should be changed

---
+++ Osiris/Assets/MyScripts/Battle Scene/BattleAbilities.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleAbilities : MonoBehaviour {
+
+	float fade = 0;
+	float fadeSpeed = 5;
+	bool spaceDown = false;
+	// Use this for initialization
+	void Start () {
+		//fade = this.GetComponent<CanvasGroup> ().alpha;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<CanvasGroup> ().alpha = fade;
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			spaceDown = true;
+
+		}
+		if (spaceDown == true) {
+			if (fade <= 0) {
+				fade += Time.deltaTime;
+			} else if (fade >= 1) {
+				fade -= Time.deltaTime;
+			}
+		}
+	}
+}
---
+++ Osiris/Assets/MyScripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 3;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal") * speed;
+		float vertical = Input.GetAxis ("Vertical") * speed;
+		transform.Translate (horizontal, vertical, 0);
+	}
+}
