3024885120afe426f35a51c509a87092bd45c480
RCM-PC\RCM <ktmead@asu.edu>
2016-09-20 23:33:07 -0700

    Saving
    Player Prefs

---
+++ Osiris/Assets/KevinBranch/GameInformation/PlayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInfo : MonoBehaviour {
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    public static string PlayerName { get; set;}
+    public static int PlayerLevel { get; set; }
+    public static int PlayerSpeed { get; set; }
+    public static int PlayerWillpower { get; set; }
+    public static float PlayerProgress { get; set; }
+    public static int PlayerStrength { get; set; }
+    public static int PlayerIntellect { get; set; }
+}
